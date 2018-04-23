b9244fc96e7cdc1c9277d4e64e1a7ded32b91e1d
Taylor Wong <Taylor Wong>
2018-04-23 07:43:44 -0400

    css

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/instructions')
+def instructions():
+    return render_template('instructions.html')
+
 if (__name__ == "__main__"):
     app.debug = True
     app.run()
